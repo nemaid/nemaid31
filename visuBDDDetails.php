d83129c481e2eb3933d2d9830946779449ac9ce6
Damien Richard <damien.richard@arondor.com>
2013-03-14 09:57:58 +0100

    Visubdd

--- visuBDDDetails.php
+++ visuBDDDetails.php
@@ -1,22 +1,20 @@
-<<<<<<< HEAD
 <?php
 /*
  * Created on 12 mars 2013
  *
- * To change the template for this generated file go to
+ * To change tde template for tdis generated file go to
  * Window - Preferences - PHPeclipse - PHP - Code Templates
  */
  
  // On inclut les pages de connection à la BDD et des fonctions
 include("connectionSQL.php");
-include("functions.php");
 
 
 $name = $_GET["specie"];
 
 
 //requete
-$result = mysql_query("select LON,
+$result = mysql_query("SELECT LON,
 	STY,
 	DGO,
 	EXPO,
@@ -52,37 +50,15 @@ $result = mysql_query("select LON,
 	GENB1,
 	GENB2,
 	GENB3
-from species, define, data where specie='$name' AND species.code_spe=define.code_spe and data.id_data=define.id_data;");
+FROM species, define, data WHERE specie='$name' AND species.code_spe=define.code_spe AND data.id_data=define.id_data;");
 
 echo "<table border='1'>
 <tr>
-<th>Species</th>
-<th>Authors</th>
-<th>Validity</th>
-</tr>";
-
-
+<th>Body lenght</th>";
 while($row = mysql_fetch_array($result))
-{
-	echo $row['LON'];
-}
-
-
-
-
-
+	{
+		echo "<td>" . $row['LON'] . "</td>";
+	}
+echo "</tr>";
+echo "</table>";
 ?>
-=======
-<?php
-/*
- * Created on 12 mars 2013
- *
- * To change the template for this generated file go to
- * Window - Preferences - PHPeclipse - PHP - Code Templates
- */
- 
-
-echo "JE TE MANGE LE MULET";
- 
-?>
->>>>>>> 4ef23e173e72d7e4733702efbba74208a28588b4
