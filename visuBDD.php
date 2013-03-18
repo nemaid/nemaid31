e33163dd36bef0b2a8629f25b778c2a3af2d8bbc
Damien Richard <damien.richard@arondor.com>
2013-03-18 18:21:39 +0100

    On avance on avance

--- visuBDD.php
+++ visuBDD.php
@@ -5,26 +5,12 @@ include("connectionSQL.php");
 include("functions.php");
 include("includes/haut.php");
 
-//on récupère l'id de connexion 
-//$id = session_id();
-//si l'id est égal à admin alors on permet l'accès au managment de la bdd
-//if (($id) == ("ciret.m@gmail.com")) {
-// création du bouton de redirection vers le mangement de la BDD
-?>
-<input type="button" name="DB Management" value="Return" onClick="javascript:document.location.href='BDDManagement.php'" />
-<?php
-//}
 
-$result = mysql_query("SELECT define.code_spe, specie, authors, years, validity
-				FROM `species`
-				LEFT OUTER JOIN `define` ON define.code_spe = species.code_spe
-				GROUP BY specie");
+$result = mysql_query("SELECT specie from species");
 
 echo "<table border='1'>
 <tr>
 <th>Species</th>
-<th>Authors</th>
-<th>Validity</th>
 </tr>";
 
 
@@ -32,17 +18,10 @@ while($row = mysql_fetch_array($result))
   {
   	
   	$nameSpecies = $row['specie'];
-  	$url = "visuBDDDetails.php?specie=";
+  	$url = "visuBDDBySpecies.php?specie=";
   	$url .= $nameSpecies;
   echo "<tr>";
-  echo "<td>" ."<a href=$url target=$target>$nameSpecies</a>" . "</td>";
-  echo "<td>" . $row['authors'] . "</td>";
-  if ($row['validity'] == 1){ 
-  	echo "<td>" . "true"  . "</td>";
-  }
-   else {
-  	echo "<td>" . "false" . "</td>";
-  	}	
+  echo "<td>" ."<a href=$url target='_blank'>$nameSpecies</a>" . "</td>";
   echo "</tr>";
   }
 echo "</table>";
--- visuBDDDetails.php
+++ visuBDDDetails.php
@@ -8,342 +8,420 @@
  
  // On inclut les pages de connection à la BDD et des fonctions
 include("connectionSQL.php");
-
+include("includes/haut.php");
 
 
 //requete
-function request($char, $name){
-	$result = mysql_query("SELECT $char FROM species, define, data WHERE specie='$name' AND species.code_spe=define.code_spe AND data.id_data=define.id_data;");
+function request($char, $idData){
+	$result = mysql_query("SELECT $char FROM data where id_data=$idData");
 	return $result;
 }
+
+
+?>
+
+<form action="updateBDD.php" method="post">
+
+
+<?php
 echo "<table border='1'>
 <tr>
 <th>Body lenght</th>";
 
-$name = $_GET["specie"];
-$result = request("LON", $name);
+$idData = $_GET["iddata"];
+echo "<input type='hidden' name='iddata' value=$idData> ";
+$result = request("LON", $idData);
 while ($row = mysql_fetch_array($result))
 {	
-	echo "<td>" . $row['LON'] . "</td>";
+	$value = $row['LON'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu1' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
 mysql_free_result($result);
 
 echo "<tr>
 <th>Stylet lenght</th>";
-$result = request("STY", $name);
+$result = request("STY", $idData);
 while($row = mysql_fetch_array($result))
-		{
-	echo "<td>" . $row['STY'] . "</td>";
-		}
+{
+	$value = $row['STY'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu2' size='2' value='$value'>"  . "</td>";
+}
 echo "</tr>";
-mysql_free_result($result);
 
+mysql_free_result($result);
 echo "<tr>
 <th>Position of dorsal gland opening</th>";
-$result = request("DGO", $name);
+$result = request("DGO", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['DGO'] . "</td>";
+	$value = $row['DGO'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu3' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
-mysql_free_result($result);
 
+mysql_free_result($result);
 echo "<tr>
 <th>Position of excretory pore</th>";
-$result = request("EXPO", $name);
+$result = request("EXPO", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['EXPO'] . "</td>";
+	$value = $row['EXPO'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu4' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 
 echo "<tr>
 <th>Width of body annules</th>";
-$result = request("BAW", $name);
+$result = request("BAW", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['BAW'] . "</td>";
+	$value = $row['BAW'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu5' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Tail length</th>";
-$result = request("TAIL", $name);
+$result = request("TAIL", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['TAIL'] . "</td>";
+	$value = $row['TAIL'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu6' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Number of tail annules (ventral side)</th>";
-$result = request("TAN", $name);
+$result = request("TAN", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['TAN'] . "</td>";
+	$value = $row['TAN'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu7' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Position of phasmids</th>";
-$result = request("PHAS", $name);
+$result = request("PHAS", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['PHAS'] . "</td>";
+	$value = $row['PHAS'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu8' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Ratio a</th>";
-$result = request("a", $name);
+$result = request("a", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['a'] . "</td>";
+	$value = $row['a'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu9' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Ratio c</th>";
-$result = request("c", $name);
+$result = request("c", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['c'] . "</td>";
+	$value = $row['c'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu10' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Ratio c bis</th>";
-$result = request("c_bis", $name);
+$result = request("c_bis", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['c_bis'] . "</td>";
+	$value = $row['c_bis'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu11' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Ratio m</th>";
-$result = request("m", $name);
+$result = request("m", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['m'] . "</td>";
+	$value = $row['m'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu12' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Ratio V</th>";
-$result = request("v", $name);
+$result = request("v", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['v'] . "</td>";
+	$value = $row['v'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu13' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Spicule lenght</th>";
-$result = request("SPIC", $name);
+$result = request("SPIC", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['SPIC'] . "</td>";
+	$value = $row['SPIC'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu14' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Presence/absence of males</th>";
-$result = request("MALES", $name);
+$result = request("MALES", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['MALES'] . "</td>";
+	$value = $row['MALES'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu15' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Labial disc	Intestinal canals (fasciculi)</th>";
-$result = request("DISC", $name);
+$result = request("DISC", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['DISC'] . "</td>";
+	$value = $row['DISC'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu16' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Labial disc	Intestinal canals (fasciculi)</th>";
-$result = request("CAN", $name);
+$result = request("CAN", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['CAN'] . "</td>";
+	$value = $row['CAN'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu17' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Habitus 1</th>";
-$result = request("HAB1", $name);
+$result = request("HAB1", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['HAB1'] . "</td>";
+	$value = $row['HAB1'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu18' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Habitus 2</th>";
-$result = request("HAB2", $name);
+$result = request("HAB2", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['HAB2'] . "</td>";
+	$value = $row['HAB2'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu19' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Lip shape 1</th>";
-$result = request("LIP1", $name);
+$result = request("LIP1", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['LIP1'] . "</td>";
+	$value = $row['LIP1'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu20' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Lip shape 2</th>";
-$result = request("LIP2", $name);
+
+$result = request("LIP2", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['LIP2'] . "</td>";
+	$value = $row['LIP2'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu21' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Fusion of inner lateral field lines (incisures) on tail 1</th>";
-$result = request("INC1", $name);
+$result = request("INC1", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['INC1'] . "</td>";
+	$value = $row['INC1'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu22' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Fusion of inner lateral field lines (incisures) on tail 2</th>";
-$result = request("INC2", $name);
+$result = request("INC2", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['INC2'] . "</td>";
+	$value = $row['INC2'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu23' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Number of lip annules 1</th>";
-$result = request("LANN1", $name);
+$result = request("LANN1", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['LANN1'] . "</td>";
+	$value = $row['LANN1'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu24' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Number of lip annules 2</th>";
-$result = request("LANN2", $name);
+$result = request("LANN2", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['LANN2'] . "</td>";
+	$value = $row['LANN2'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu25' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Number of lip annules 3</th>";
-$result = request("LANN3", $name);
+$result = request("LANN3", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['LANN3'] . "</td>";
+	$value = $row['LANN3'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu26' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Shape of anterior face of stylet knobs 1</th>";
-$result = request("KBS1", $name);
+$result = request("KBS1", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['KBS1'] . "</td>";
+	$value = $row['KBS1'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu27' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Shape of anterior face of stylet knobs 2</th>";
-$result = request("KBS2", $name);
+$result = request("KBS2", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['KBS2'] . "</td>";
+	$value = $row['KBS2'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu28' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Shape of anterior face of stylet knobs 3</th>";
-$result = request("KBS3", $name);
+$result = request("KBS3", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['KBS3'] . "</td>";
+	$value = $row['KBS3'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu29' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Shape of tail 1</th>";
-$result = request("TSH1", $name);
+$result = request("TSH1", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['TSH1'] . "</td>";
+	$value = $row['TSH1'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu30' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Shape of tail 2</th>";
-$result = request("TSH2", $name);
+$result = request("TSH2", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['TSH2'] . "</td>";
+	$value = $row['TSH2'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu31' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Shape of tail 3</th>";
-$result = request("TSH3", $name);
+$result = request("TSH3", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['TSH3'] . "</td>";
+	$value = $row['TSH3'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu32' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Shape of tail 4</th>";
-$result = request("TSH4", $name);
+$result = request("TSH4", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['TSH4'] . "</td>";
+	$value = $row['TSH4'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu33' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Genital branches 1</th>";
-$result = request("GENB1", $name);
+$result = request("GENB1", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['GENB1'] . "</td>";
+	$value = $row['GENB1'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu34' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Genital branches 2</th>";
-$result = request("GENB2", $name);
+$result = request("GENB2", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['GENB2'] . "</td>";
+	$value = $row['GENB2'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu35' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
+
 mysql_free_result($result);
 echo "<tr>
 <th>Genital branches 3</th>";
-$result = request("GENB3", $name);
+$result = request("GENB3", $idData);
 while($row = mysql_fetch_array($result))
 {
-	echo "<td>" . $row['GENB3'] . "</td>";
+	$value = $row['GENB3'];
+	echo "<td>" . "<INPUT TYPE='text' name='nbmenu36' size='2' value='$value'>"  . "</td>";
 }
 echo "</tr>";
 mysql_free_result($result);
@@ -351,3 +429,6 @@ echo "</table>";
 
 
 ?>
+
+<button type = 'submit' name ='update' action="updateBDD.php" target="_blank">Update</button>
+</form>
